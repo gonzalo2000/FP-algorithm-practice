f388661d9b75cb7424af4e3985723069e4994903
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-04-15 21:55:16 -0400

    Tweak cards challenge

--- cards.rb
+++ cards.rb
@@ -16,32 +16,8 @@ class Card
 end
 
 class Deck
-    # def initialize
-    #     suits = [:clubs, :spades, :hearts, :diamonds]
-    #     deck = []
-    #     suits.each do |suit|
-    #         (1..13).each {|rank| deck << [suit, rank]}
-    #     end
-        
-    #     @all_cards = []
-    #     deck.each do |card|
-    #         @all_cards << Card.new(card[1], card[0])
-    #     end
-    # end
-    
-
-    # def initialize 
-    #   suits = [:clubs, :spades, :hearts, :diamonds] 
-    #   @all_cards = [] 
-    #   ranks = ['Ace', *(2..10), 'Jack', 'Queen', 'King'] 
-    #   ranks.each do |rank| 
-    #     @all_cards << Card.new(rank, suit) 
-    #   end 
-    # end
-
     def initialize
         suits = [:clubs, :spades, :hearts, :diamonds]
-        deck = []
         @all_cards = []
         ranks = ['Ace', *(2..10), 'Jack', 'Queen', 'King'] 
         suits.each do |suit|
