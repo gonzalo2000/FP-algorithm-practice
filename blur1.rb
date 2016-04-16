ec1dd7cdb8554173b2170932924334d3f248c814
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-04-15 20:57:24 -0400

    Complete Image Blur #1

--- blur1.rb
+++ blur1.rb
@@ -1,1 +1,23 @@
-puts "hi, caper"
+class Image
+  attr_accessor :array
+  
+  def initialize(array)
+    self.array = array
+  end
+  
+  def output_image
+    self.array.each do |item|
+      item.each {|x| print x}
+      puts
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
+
