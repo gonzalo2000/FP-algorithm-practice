765452534f0bd67428196d0cbe5f90436d16beb8
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-04-17 17:13:00 -0400

    Add hack for blur2 challenge

---
+++ blur2.rb
@@ -0,0 +1,26 @@
+a = [[0,0,0,0],
+     [0,0,1,0],
+     [0,0,0,0],
+     [0,0,0,0]
+    ]
+
+#create a placeholder copy of the array; all elements == nil
+new = Array.new(a.length).map! {|element| element = Array.new(a[0].length)}
+
+#scan original array for 1; map crosses into new array if found
+a.each_with_index do |m, n| # n == index of main array
+    m.each_with_index do |x, y| # y == index of subarray
+        if x == 1
+            new[n][y] = 1
+            new[n][y+1] = 1 unless (a[n][y+1]).nil? #right
+            new[n][y-1] = 1 unless (a[n][y-1]).nil? #left
+            new[n-1][y] = 1 unless (a[n-1][y]).nil? #top
+            new[n+1][y] = 1 unless (a[n+1][y]).nil? #bottom
+        end
+    end
+end
+
+#changes nils into 0s and returns new array
+new.each do |element|
+  element.map! {|x| x == nil ? x = 0 : x = 1 }
+end
