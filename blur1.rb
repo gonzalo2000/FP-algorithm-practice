e5beaa11b7cf6ae5216f70063a08b85520e4145f
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-04-24 15:29:11 -0400

    Pass tests for blur3, incorporated to blur1 file

--- blur1.rb
+++ blur1.rb
@@ -32,11 +32,69 @@ class Image
     end
     new
   end
+
+  #Blur 3 exercise
+  def blur(distance)
+    cloned = self.array.map(&:clone) #changes will map into array clone
+    
+    n = distance # maximum vertical/horizontal distance
+
+    self.array.each.with_index do |whole_row, row|
+      whole_row.each.with_index do |cell, column|
+        if cell == 1
+          
+          #central column & right side
+          row_path = row - n
+          column_path = column
+          repetition = 2 * n + 1
+          counter = 0
+          (n+1).times do 
+            (repetition).times do
+              unless (row_path > cloned.length - 1 ) || cloned[row_path][column_path].nil? 
+                cloned[row_path][column_path] = 1
+              end
+              row_path += 1
+            end
+            counter += 1
+            repetition -= 2
+            column_path += 1
+            row_path = row - n + counter
+          end
+
+          #central column & left side
+          row_path = row - n
+          column_path = column
+          repetition = 2 * n + 1
+          counter = 0
+          (n+1).times do 
+            (repetition).times do
+              unless (row_path > cloned.length - 1 ) || cloned[row_path][column_path].nil? 
+                cloned[row_path][column_path] = 1
+              end
+              row_path += 1
+            end
+            counter -= 1
+            repetition -= 2
+            column_path -= 1
+            row_path = row - n - counter
+          end
+        end
+      end
+    end
+    cloned # return the array with all the mapped changes
+  end
   
 end
 
-# image = Image.new([
-#   [0, 0, 0, 0],
-#   [0, 1, 0, 0],
-#   [0, 0, 0, 0],
-#   [0, 0, 0, 0]])
+# image = Image.new([[0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,1,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [0,0,0,0,0,0,0,0,0],
+#                    [1,0,0,0,0,0,0,0,1]])
