faf877f5bbec5a103fb64d120c9a479d5c84752e
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2016-05-02 20:23:59 -0400

    Refactor blur1 transform method, outline blur method iteratively

--- blur1.rb
+++ blur1.rb
@@ -5,96 +5,50 @@ class Image
     self.array = array
   end
   
-  # Blur 1 exercise
   def output_image
     self.array.each do |item|
       puts item.join
     end
   end
 
-  # Blur 2 exercise
   def transform 
-    new = self.array.map(&:clone)
+    cloned = self.array.map(&:clone)
 
     #scan original array for 1; map crosses into clone if found
-    self.array.each_with_index do |m, n| # n == index of main array
-        m.each_with_index do |x, y| # y == index of subarray
-          if x == 1 
-                new[n][y] = 1
-                new[n][y+1] = 1 unless (new[n][y+1]).nil? #right
-                new[n][y-1] = 1 unless (new[n][y-1]).nil? || y == 0 #left
-                new[n-1][y] = 1 unless (new[n-1][y]).nil? || n == 0 #top
-                unless n == (new.length - 1)
-                  new[n+1][y] = 1 unless (new[n+1][y]).nil? || n == (new.size - 1) #bottom
-                end
-            end
-        end
-    end
-    new
-  end
-
-  #Blur 3 exercise
-  def blur(distance)
-    cloned = self.array.map(&:clone) #changes will map into array clone
-    
-    n = distance # maximum vertical/horizontal distance
-
-    self.array.each.with_index do |whole_row, row|
-      whole_row.each.with_index do |cell, column|
-        if cell == 1
-          
-          #central column & right side
-          row_path = row - n
-          column_path = column
-          repetition = 2 * n + 1
-          counter = 0
-          (n+1).times do 
-            (repetition).times do
-              unless (row_path > cloned.length - 1 ) || cloned[row_path][column_path].nil? 
-                cloned[row_path][column_path] = 1
-              end
-              row_path += 1
-            end
-            counter += 1
-            repetition -= 2
-            column_path += 1
-            row_path = row - n + counter
-          end
-
-          #central column & left side
-          row_path = row - n
-          column_path = column
-          repetition = 2 * n + 1
-          counter = 0
-          (n+1).times do 
-            (repetition).times do
-              unless (row_path > cloned.length - 1 ) || cloned[row_path][column_path].nil? 
-                cloned[row_path][column_path] = 1
-              end
-              row_path += 1
-            end
-            counter -= 1
-            repetition -= 2
-            column_path -= 1
-            row_path = row - n - counter
-          end
-        end
+    self.array.each.with_index do |row, row_index|
+    row.each.with_index do |cell, col|
+      if cell == 1
+        cloned[row_index][col+1] = 1 unless col+1 >= row.length #copy right
+        cloned[row_index+1][col] = 1 unless row_index+1 >= cloned.length # copy down
+        cloned[row_index][col-1] = 1 unless col.zero? # copy left
+        cloned[row_index-1][col] = 1 unless row_index.zero? #copy up
       end
     end
-    cloned # return the array with all the mapped changes
   end
-  
+    cloned
+  end
+
+  def blur(n) #not working at present
+    blurred = Image.new(self)
+    n.times do
+    blurred = blurred.transform
+  end
+  blurred
+  end
+    
 end
 
-# image = Image.new([[0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,1,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [0,0,0,0,0,0,0,0,0],
-#                    [1,0,0,0,0,0,0,0,1]])
+image = Image.new([[0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,1,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0]])
+
+y = image.transform
